202e98997fa4d42b81e7c400a86bde4579aa2d90
shimingjiao <shimingjiao@xiaokale.onaliyun.com>
2018-04-16 17:28:28 +0800

    xiaokale

--- js/public.js
+++ js/public.js
@@ -4,7 +4,7 @@
  */
 (function (win, doc) {
     function change() {
-        doc.documentElement.style.fontSize = 100 * doc.documentElement.clientWidth / 750 + 'rem';
+        doc.documentElement.style.fontSize = 100 * doc.documentElement.clientWidth / 750 + 'px';
     }
     change();
     win.addEventListener('resize', change, false);
